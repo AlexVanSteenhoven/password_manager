616889802445307192a7310498a48a7f854fb1f1
Alex van Steenhoven <alex.steenhoven@gmail.com>
2021-03-19 15:37:12 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,45 @@
+const electon = require("electron");
+const app = electon.app;
+const browserWindow = electon.BrowserWindow;
+const path = require("path");
+
+const createWindow = () => {
+  let startupView = new browserWindow({
+    // Setting up window sizes
+    width: 500,
+    height: 900,
+    maxHeight: 900,
+    maxWidth: 500,
+
+    // Other options
+    frame: false,
+    resizable: false,
+    titleBarStyle: "hidden",
+    backgroundColor: "#202121",
+    maximizable: false,
+
+    // prefferences
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+    },
+  });
+
+  startupView.loadURL(
+    "file://" + path.join(__dirname + "/frontend/login.html")
+  );
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
